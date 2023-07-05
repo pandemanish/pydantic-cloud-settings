ac3b339cd25b9eb05ad7c607e542677606967c3d
Manish Pande <manish.pande@forgeahead.iowq>
2023-07-05 14:41:10 -0700

    Updated pydantic version ([>= 1.7.0, < 1.8] to 1.10.11)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(install_requires=[
-    "pydantic>=2.0.2",
+    "pydantic>=1.8.1,<2.0.0",
     "python-dotenv>=1.0.0",
     "google-cloud-secret-manager>=2.16.2,<3",
 ])
