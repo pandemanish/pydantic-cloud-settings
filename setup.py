951b5eaa427a1e2d8f4b458637bcc543f3ef214c
Manish Pande <manish.pande@forgeahead.iowq>
2023-07-05 11:39:52 -0700

    Updated dotenv version ([>=0.17 and <0.18] to 1.0.0)

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import setuptools
 
 setuptools.setup(install_requires=[
     "pydantic>=1.7.0,<1.8",
-    "python-dotenv>=0.17,<0.18",
+    "python-dotenv>=1.0.0",
     "google-cloud-secret-manager>=2.2.0,<3",
 ])
